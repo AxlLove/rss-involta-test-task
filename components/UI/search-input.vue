f75ea50db903b66bf7cb98f1a6958ea4736c14bc
Aleksey Kitaev <axlstarxx@gmail.com>
2023-02-19 20:46:15 +0300

    feat: add header component

---
+++ components/UI/search-input.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="input__container">
+    <input :value="value" @input="updateInput" class="input">
+    <div class="icon"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search-input',
+  props: {
+    value: [String, Number]
+  },
+  methods: {
+    updateInput (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .input__container {
+    position: relative;
+    width: 321px;
+    height: 40px;
+  }
+  .input {
+    width: 100%;
+    height: 100%;
+    border: none;
+    opacity: 0.5;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05), 0px 1px 4px rgba(0, 0, 0, 0.05);
+    background: #FFFFFF;
+    border-radius: 3px;
+
+  }
+  .icon {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    background: url("../../static/input-icon.svg");
+    right: 13px;
+    top: 10px;
+    bottom: 10px;
+  }
+  input:focus {
+    border: 1px solid black;
+    opacity: .3;
+  }
+</style>
--- components/v-header.vue
+++ components/v-header.vue
@@ -1,15 +1,63 @@
 <template>
-  <header>
-    <h1>Список новостей</h1>
+  <header class="header">
+    <div class="header__container">
+      <h1 class="header__title">Список новостей</h1>
+      <button @click="test" class="header__reset-button"/>
+    </div>
+    <search-input v-model="searchQuery"/>
   </header>
 </template>
 
 <script>
+import MyInput from "~/components/UI/search-input";
+import SearchInput from "~/components/UI/search-input";
 export default {
-  name: 'v-header'
+  name: 'v-header',
+  components: {SearchInput, MyInput},
+  data() {
+    return {
+      a: 225,
+      searchQuery: ''
+    }
+  },
+  methods: {
+    test() {
+      console.log(this.a)
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .header {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 170px;
+    padding: 36px 0;
+    border-bottom: 1px solid #E5E5E5;
+  }
+  .header__container {
+    display: flex;
+    gap: 30px;
+    align-items: center;
+  }
+  .header__title {
+    font-size: 36px;
+    line-height: 41px;
+  }
+  .header__reset-button {
+    width: 40px;
+    height: 40px;
+    border-radius: 50px;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05), 0px 1px 4px rgba(0, 0, 0, 0.05);
+    border: none;
+    background-color: #FFFFFF;
+    background-image: url("../static/reset-button-icon.svg");
+    background-position: center;
+    background-repeat: no-repeat;
+    cursor: pointer;
+  }
+  .header__reset-button:hover {
+    opacity: 0.8;
+  }
 </style>
